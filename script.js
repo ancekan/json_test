5cf918db05f2ca94313d8db51dc90536354d3ef1
Ance Kancere <58723696+ancekan@users.noreply.github.com>
2022-01-09 20:31:08 +0000

    table reading added

--- script.js
+++ script.js
@@ -1,11 +1,38 @@
+//project code by dcode on youtube https://youtu.be/qBg8IB3u28s
+
 async function loadIntoTable(url, table) {
     const tableHead = table.querySelector("thead");
     const tableBody = table.querySelector("tbody");
     const response = await fetch(url);
-    const data = await response.json();
+    const { headers, rows } = await response.json();
 
-    console.log(data);
+    //Notirit tabulu
+    tableHead.innerHTML = "<tr></tr>";
+    tableBody.innerHTML = "";
 
+    //Nolasit tabulas galveni
+    for (const headerText of headers) {
+      const headerElement = document.createElement("th");
+
+      headerElement.textContent = headerText;
+      tableHead.querySelector("tr").appendChild(headerElement);
+    }
+
+    //Nolasit tabulas rindas
+    for (const row of rows) {
+      const rowElement = document.createElement("tr");
+
+      //Nolasit rindas elementus
+      for (const cellText of row) {
+        const cellElement = document.createElement("td");
+
+        cellElement.textContent = cellText;
+        rowElement.appendChild(cellElement);
+      }
+
+      tableBody.appendChild(rowElement);
+
+    }
 }
 
 loadIntoTable("./data.json", document.querySelector("table"));
