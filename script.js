550474c7b22170d50d260d0f6be1a8327fbd0d0c
Ance Kancere <58723696+ancekan@users.noreply.github.com>
2022-01-24 07:33:50 +0000

    pievienots 2.variants datu nolasīšanai

--- script.js
+++ script.js
@@ -1,38 +1,80 @@
+//1.variants
 //project code by dcode on youtube https://youtu.be/qBg8IB3u28s
 
-async function loadIntoTable(url, table) {
-    const tableHead = table.querySelector("thead");
-    const tableBody = table.querySelector("tbody");
-    const response = await fetch(url);
-    const { headers, rows } = await response.json();
+// async function loadIntoTable(url, table) {
+//     //const tableHead = table.querySelector("thead");
+//     const tableBody = table.querySelector("tbody");
+//     const response = await fetch(url);
+//     const { rows } = await response.json();
 
-    //Notirit tabulu
-    tableHead.innerHTML = "<tr></tr>";
-    tableBody.innerHTML = "";
+//     //Notirit tabulu
+//     //tableHead.innerHTML = "<tr></tr>";
+//     tableBody.innerHTML = "";
 
-    //Nolasit tabulas galveni
-    for (const headerText of headers) {
-      const headerElement = document.createElement("th");
+//     //Nolasit tabulas galveni
+//     // for (const headerText of headers) {
+//     //   const headerElement = document.createElement("th");
 
-      headerElement.textContent = headerText;
-      tableHead.querySelector("tr").appendChild(headerElement);
+//     //   headerElement.textContent = headerText;
+//     //   tableHead.querySelector("tr").appendChild(headerElement);
+//     // }
+
+//     //Nolasit tabulas rindas
+//     for (const row of rows) {
+//       const rowElement = document.createElement("tr");
+
+//       //Nolasit rindas elementus
+//       for (const cellText of row) {
+//         const cellElement = document.createElement("td");
+
+//         cellElement.textContent = cellText;
+//         rowElement.appendChild(cellElement);
+//       }
+
+//       tableBody.appendChild(rowElement);
+
+//     }
+// }
+
+// //funkcijas izsauksana
+// loadIntoTable("./data.json", document.querySelector("table"));
+
+
+
+
+//2.variants
+//https://youtu.be/FN_ffvw_ksE
+
+function fetchData() {
+  fetch("./data2.json").then(response => {
+    if (!response.ok) {
+      throw Error("Kļūda!");
     }
 
-    //Nolasit tabulas rindas
-    for (const row of rows) {
-      const rowElement = document.createElement("tr");
+    return response.json();
+  })
 
-      //Nolasit rindas elementus
-      for (const cellText of row) {
-        const cellElement = document.createElement("td");
+  .then(data => {
+    const tabula = data.map(persona => {
+      return `
+          <tr>
+            <td>${persona.vards}</td>
+            <td>${persona.uzvards}</td>
+            <td>${persona.pilseta}</td>
+          </tr>
+      `;
+    })
+    .join("");
 
-        cellElement.textContent = cellText;
-        rowElement.appendChild(cellElement);
-      }
+    document.querySelector('#showdata').innerHTML = tabula;
+  })
 
-      tableBody.appendChild(rowElement);
-
-    }
+  .catch(error => {
+    console.log(error);
+  });
 }
 
-loadIntoTable("./data.json", document.querySelector("table"));
+fetchData();
+
+
+
